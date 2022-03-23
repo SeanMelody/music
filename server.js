a7334226233494530300de3b01ba9dcf1637f49c
Sean Melody <seandmelody@gmail.com>
2022-03-22 17:46:16 -0700

    packages added

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
-console.log("hi")
+// Required Consts!
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// Set up for Heroku or Port 5000 cause I'm crazy!
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+// Optimize for Heroku
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+// Connect to the mongo database!
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project-3", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+// Let the user know the server is running, and which port.  Yeay!
+app.listen(PORT, () => {
+    console.log(`listening at http://localhost:${PORT}`);
+});
