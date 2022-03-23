6a5a02b3418f9fdad3e5b378e4f5613ca15119c2
Sean Melody <seandmelody@gmail.com>
2022-03-22 18:52:15 -0700

    temp landing page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>My Music App</h1>
+      <br></br>
+      <h3>Website Under Construction</h3>
+      <h4>Contact Developer
+        <a href="mailto: dzesean@gmail.com">
+          Here
+        </a></h4>
     </div>
   );
 }
